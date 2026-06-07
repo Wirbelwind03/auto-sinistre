c096c08ea016422d462336acf12fc379ae297375
WirbelWind03 <dylanmei19@gmail.com>
2026-06-07 06:27:10 +0200

    Validation check

--- server/src/main/java/com/github/wirbelwind03/autosinistre/service/AuthService.java
+++ server/src/main/java/com/github/wirbelwind03/autosinistre/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.github.wirbelwind03.autosinistre.service;
 
+import com.github.wirbelwind03.autosinistre.exception.InvalidEmailException;
+import com.github.wirbelwind03.autosinistre.exception.UserAlreadyExistException;
 import com.github.wirbelwind03.autosinistre.model.dto.request.AuthRequestDTO;
 import com.github.wirbelwind03.autosinistre.model.dto.response.AuthResponseDTO;
 import com.github.wirbelwind03.autosinistre.model.dto.request.RegisterRequestDTO;
@@ -9,6 +11,7 @@ import com.github.wirbelwind03.autosinistre.model.enums.RoleEnum;
 import com.github.wirbelwind03.autosinistre.repository.RoleRepository;
 import com.github.wirbelwind03.autosinistre.repository.UserRepository;
 import com.github.wirbelwind03.autosinistre.security.JwtUtil;
+import com.github.wirbelwind03.autosinistre.util.ValidationUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -29,19 +32,25 @@ public class AuthService {
 
     public AuthResponseDTO register(RegisterRequestDTO request){
         if (userRepository.existsByEmail(request.getEmail())){
-            throw new IllegalArgumentException("Un compte existe déjà avec cette email.");
+            throw new UserAlreadyExistException();
         }
 
-        Optional<Role> optionalRole = roleRepository.findByName(RoleEnum.CLIENT);
+        if (!ValidationUtils.isValidEmail(request.getEmail())){
+            throw new InvalidEmailException();
+        }
 
-        if (optionalRole.isEmpty()) {
+        // Mettre le role client au nouveau utilisateur
+        Optional<Role> role = roleRepository.findByName(RoleEnum.CLIENT);
+        if (role.isEmpty()) {
             return null;
         }
 
         User user = User.builder()
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
-                .role(optionalRole.get())
+                .role(role.get())
+                .name(request.getName())
+                .firstName(request.getFirstName())
                 .build();
 
         userRepository.save(user);
