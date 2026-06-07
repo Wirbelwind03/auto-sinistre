72728b43392d9e794caa592ed9ede36843ac8a72
WirbelWind03 <dylanmei19@gmail.com>
2026-06-07 18:04:31 +0200

    Refactorisation, et ajout de l'inscription

--- client/src/stores/auth.js
+++ client/src/stores/auth.js
@@ -5,24 +5,40 @@ import api from '@/services/api'
 export const useAuthStore = defineStore('auth', () => {
     const token = ref(localStorage.getItem('token') || null)
     const userEmail  = ref(localStorage.getItem('email') || null)
+    const role  = ref(localStorage.getItem('role') || null)
     
     const isAuthenticated = computed(() => !!token.value)
 
     async function login(email, password) {
-        const { data } = await api.post('/auth/login', { email, password })
-        token.value = data.token
-        userEmail.value = data.email
-        localStorage.setItem('token', data.token)
-        localStorage.setItem('email', data.email)
+        const response = await api.post('/auth/login', { email, password })
+        getAuthResponse(response.data)
+    }
+
+    async function register(email, password, name, firstName) {
+        const response = await api.post('/auth/register', { email, password, name, firstName })
+        getAuthResponse(response.data)
     }
 
     function logout(){
         token.value = null
         userEmail.value  = null
+        // role.value  = null
+
         localStorage.removeItem('token')
         localStorage.removeItem('email')
-
+        // localStorage.removeItem('role')
     }
 
-    return { token, userEmail, isAuthenticated, login, logout }
+    function getAuthResponse(authResponse){
+        token.value = authResponse.token
+        userEmail.value = authResponse.email
+        // role.value = authResponse.role
+        
+        localStorage.setItem('token', authResponse.token)
+        localStorage.setItem('email', authResponse.email)
+        // localStorage.setItem('role', authResponse.role)
+    }
+
+
+    return { token, userEmail, isAuthenticated, login, register, logout }
 })
