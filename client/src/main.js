da1c64106df0135351364b9a1c14de0be5acc582
WirbelWind03 <dylanmei19@gmail.com>
2026-06-08 05:49:26 +0200

    Change theme en dark

--- client/src/main.js
+++ client/src/main.js
@@ -7,7 +7,7 @@ import App from './App.vue'
 import router from './router'
 
 const vuetify = createVuetify({
-  theme: { defaultTheme: 'light' }
+  theme: { defaultTheme: 'dark' }
 })
 const pinia = createPinia()
 
