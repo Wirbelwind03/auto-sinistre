45e2367cfdb5c014359c8014058875e3c5eac6b3
WirbelWind03 <dylanmei19@gmail.com>
2026-06-09 19:09:20 +0200

    Enlever élément inutile

--- client/src/components/vehicle/AddCard.vue
+++ client/src/components/vehicle/AddCard.vue
@@ -14,22 +14,20 @@
 </style>
 
 <template>
-    <v-col cols="12" sm="6" md="4">
-        <v-card 
-            class="add-card d-flex align-center justify-center" 
-            rounded="xl" 
-            elevation="0"
-            @click="$emit('open-add-dialog')"
-        >
-            <v-card-text class="text-center pa-8">
-            <v-avatar color="blue-lighten-4" size="56" class="mb-3">
-                <v-icon color="blue-darken-2" size="28">mdi-plus</v-icon>
-            </v-avatar>
-            <div class="text-body-1 font-weight-bold text-blue-darken-2 mb-1">Ajouter un véhicule</div>
-            <div class="text-caption text-medium-emphasis">Enregistrez un nouveau véhicule assuré</div>
-            </v-card-text>
-        </v-card>
-    </v-col>
+    <v-card 
+        class="add-card d-flex align-center justify-center" 
+        rounded="xl" 
+        elevation="0"
+        @click="$emit('open-add-dialog')"
+    >
+        <v-card-text class="text-center pa-8">
+        <v-avatar color="blue-lighten-4" size="56" class="mb-3">
+            <v-icon color="blue-darken-2" size="28">mdi-plus</v-icon>
+        </v-avatar>
+        <div class="text-body-1 font-weight-bold text-blue-darken-2 mb-1">Ajouter un véhicule</div>
+        <div class="text-caption text-medium-emphasis">Enregistrez un nouveau véhicule assuré</div>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script setup>
