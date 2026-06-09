4eb166661247abffbe531b680c52e7b4f64d7bb1
WirbelWind03 <dylanmei19@gmail.com>
2026-06-09 06:29:32 +0200

    Ajout d'une table pour les marques

---
+++ server/src/main/resources/db/migration/V3__add_brands.sql
@@ -0,0 +1,22 @@
+-- V3__insert_brands.sql
+
+INSERT INTO brands (name, is_active) VALUES
+('Audi', true),
+('BMW', true),
+('Citroën', true),
+('Dacia', true),
+('Fiat', true),
+('Ford', true),
+('Honda', true),
+('Hyundai', true),
+('Kia', true),
+('Mercedes-Benz', true),
+('Nissan', true),
+('Opel', true),
+('Peugeot', true),
+('Renault', true),
+('Seat', true),
+('Skoda', true),
+('Toyota', true),
+('Volkswagen', true),
+('Volvo', true);
