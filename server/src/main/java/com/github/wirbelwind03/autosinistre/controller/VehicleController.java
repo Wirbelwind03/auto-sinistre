0f51e0286fa81db2514f398c07b5ff7c2c7feb23
Wirbelwind03 <dylanmei19@gmail.com>
2026-06-10 05:30:50 +0200

    Récupération des types de carburant

--- server/src/main/java/com/github/wirbelwind03/autosinistre/controller/VehicleController.java
+++ server/src/main/java/com/github/wirbelwind03/autosinistre/controller/VehicleController.java
@@ -1,8 +1,10 @@
 package com.github.wirbelwind03.autosinistre.controller;
 
+import com.github.wirbelwind03.autosinistre.model.dto.request.FuelTypeRequestDTO;
 import com.github.wirbelwind03.autosinistre.model.dto.request.VehicleAddRequestDTO;
 import com.github.wirbelwind03.autosinistre.model.entity.User;
 import com.github.wirbelwind03.autosinistre.model.entity.Vehicle;
+import com.github.wirbelwind03.autosinistre.model.enums.FuelTypeEnum;
 import com.github.wirbelwind03.autosinistre.service.VehicleService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -31,5 +34,13 @@ public class VehicleController {
     public ResponseEntity<List<Vehicle>> getMyVehicles(Authentication authentication){
         User owner = (User)authentication.getPrincipal();
         return ResponseEntity.ok(vehicleService.getVehiculesByUser(owner.getId()));
+    }
+
+    @GetMapping("/fuel-types")
+    public ResponseEntity<List<FuelTypeRequestDTO>> getFuelTypes(){
+        List<FuelTypeRequestDTO> fuelTypes = Arrays.stream(FuelTypeEnum.values())
+                .map(f -> new FuelTypeRequestDTO(f.name(), f.getLabel()))
+                .toList();
+        return ResponseEntity.ok(fuelTypes);
     }
 }
