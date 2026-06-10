85d51fe3e3309bc5687dba04a2edf3c5ecaf32cb
Wirbelwind03 <dylanmei19@gmail.com>
2026-06-10 05:31:41 +0200

    Fixage du Invalid Cors request,  et remplissage auto de la liste pgadmin

--- server/src/main/java/com/github/wirbelwind03/autosinistre/config/SecurityConfig.java
+++ server/src/main/java/com/github/wirbelwind03/autosinistre/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.github.wirbelwind03.autosinistre.config;
 
 import com.github.wirbelwind03.autosinistre.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -23,6 +24,9 @@ import java.util.List;
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
 
     final private JwtAuthenticationFilter jwtAuthenticationFilter;
 
@@ -58,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(allowedOrigins));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
