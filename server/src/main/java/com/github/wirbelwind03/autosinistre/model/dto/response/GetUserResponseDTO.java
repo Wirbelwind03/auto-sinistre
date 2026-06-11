b01aa6308df293c13aee0d6e4ce06a8a28ec9f95
Wirbelwind03 <dylanmei19@gmail.com>
2026-06-11 22:21:00 +0200

    Ajout DTO pour un utilisateur

--- server/src/main/java/com/github/wirbelwind03/autosinistre/model/dto/response/GetUserResponseDTO.java
+++ server/src/main/java/com/github/wirbelwind03/autosinistre/model/dto/response/GetUserResponseDTO.java
@@ -1,4 +1,14 @@
 package com.github.wirbelwind03.autosinistre.model.dto.response;
 
+import com.github.wirbelwind03.autosinistre.model.entity.Role;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class GetUserResponseDTO {
+    private String email;
+    private String firstName;
+    private String name;
+    private String role;
 }
